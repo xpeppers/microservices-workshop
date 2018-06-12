c7e77464238999924851babab11948a378f4132e
JoeBew42 <joebew42@gmail.com>
2018-06-12 15:17:32 +0200

    consume orderpaid event

--- notifications/src/main/java/com/xpeppers/consumer/RabbitMQEventConsumer.java
+++ notifications/src/main/java/com/xpeppers/consumer/RabbitMQEventConsumer.java
@@ -1,11 +1,14 @@
 package com.xpeppers.consumer;
 
+import com.google.gson.Gson;
 import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.DefaultConsumer;
 import com.rabbitmq.client.Envelope;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
 
 class RabbitMQEventConsumer extends DefaultConsumer {
     RabbitMQEventConsumer(Channel channel) {
@@ -14,7 +17,16 @@ class RabbitMQEventConsumer extends DefaultConsumer {
 
     @Override
     public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
-        String message = new String(body, "UTF-8");
-        System.out.println(" [x] Received '" + message + "'");
+        Map payload = fromJson(body);
+
+        String orderId = (String) payload.get("orderId");
+
+        System.out.println(" [x] Received '" + payload + "'");
+        System.out.println(" [x] Send mail notification for order: '" + orderId + "'");
+    }
+
+    private Map fromJson(byte[] body) throws UnsupportedEncodingException {
+        return new Gson()
+                .fromJson(new String(body, "UTF-8"), Map.class);
     }
 }
