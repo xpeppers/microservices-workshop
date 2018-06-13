928761136a2a755ba1e393f8da1038f7fa52fb75
JoeBew42 <joebew42@gmail.com>
2018-06-13 11:26:12 +0200

    add the build phase

--- monolith/Jenkinsfile.groovy
+++ monolith/Jenkinsfile.groovy
@@ -3,22 +3,22 @@ node {
 
     stage("Checkout") {
         checkout scm
-        sh "cd monolith"
     }
-    stage("Unit tests") {
-        sh "pwd"
-        echo "RUNNING UNIT TESTS"
-    }
-    stage("Integration tests") {
-        echo "RUNNING UNIT TESTS"
-    }
-    stage("Build artifact") {
-        echo "BUILD ARTIFACT"
-    }
-    stage("UAT") {
-        echo "RUNNING UAT"
-    }
-    stage("Store Artifact") {
-        echo "STORE ARTIFACT"
+    dir("monolith") {
+        stage("Unit tests") {
+            echo "RUNNING UNIT TESTS"
+        }
+        stage("Integration tests") {
+            echo "RUNNING UNIT TESTS"
+        }
+        stage("Build artifact") {
+            sh "${mvnHome}/bin/mvn clean package"
+        }
+        stage("UAT") {
+            echo "RUNNING UAT"
+        }
+        stage("Store Artifact") {
+            archiveArtifacts artifacts: 'target/monolith-1.0-SNAPSHOT-jar-with-dependencies.jar', fingerprint: true
+        }
     }
 }
