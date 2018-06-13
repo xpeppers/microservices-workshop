46db99026bdbfb9a58c6e6ff6f36d0dab115f7f4
JoeBew42 <joebew42@gmail.com>
2018-06-13 11:30:13 +0200

    add docker build

--- monolith/Jenkinsfile.groovy
+++ monolith/Jenkinsfile.groovy
@@ -13,12 +13,14 @@ node {
         }
         stage("Build artifact") {
             sh "${mvnHome}/bin/mvn clean package"
+            sh "docker build -t joebew42/commerce.monolith ."
         }
         stage("UAT") {
             echo "RUNNING UAT"
         }
         stage("Store Artifact") {
             archiveArtifacts artifacts: 'target/monolith-1.0-SNAPSHOT-jar-with-dependencies.jar', fingerprint: true
+            echo "TODO: Push to Docker Registry ..."
         }
     }
 }
